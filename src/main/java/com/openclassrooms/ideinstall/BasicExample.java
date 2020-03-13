d45792c3d9d13fc756ab2fa3546043d80baea339
Velkyas <51044449+Velkyas@users.noreply.github.com>
2020-03-13 14:55:48 +0100

    Update BasicExample.java

--- src/main/java/com/openclassrooms/ideinstall/BasicExample.java
+++ src/main/java/com/openclassrooms/ideinstall/BasicExample.java
@@ -3,8 +3,8 @@ package com.openclassrooms.ideinstall;
 public class BasicExample {
 
 	public static void main(String[] args) {
-		final int index;
-		for(i=0; i>=0; i++) {
+		final i index;
+		for(i=0; i>=0 i++); {
 			System.out.println("Hello bugs !"):
 		}
 	}
